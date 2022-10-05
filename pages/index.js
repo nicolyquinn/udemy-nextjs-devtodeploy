10f8f0ae3b3d9b2b42469437b7c5c36a1cab8959
nikicms <ncnikims@gmail.com>
2022-10-05 19:43:12 -0300

    Added the Showcase to Homepage

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import styles from "../styles/Home.module.css";
-import Layout from "../components/Layout";
+import styles from "@/styles/Home.module.css";
+import Layout from "@/components/Layout";
 
 export default function Home() {
   return (
